1935edad25988dda8c7314c6429b46bd31f75f83
Krint243 <krint243@gmail.com>
2021-08-10 22:51:24 +0300

    Java2 (Lesson 7)

---
+++ src/Lesson7/apps/ServerApp.java
@@ -0,0 +1,10 @@
+package Lesson7.apps;
+
+import Lesson7.server.ChatServer;
+
+
+public class ServerApp {
+    public static void main(String[] args) {
+        new ChatServer();
+    }
+}
---
+++ src/Lesson7/clients/ClientOne.java
@@ -0,0 +1,10 @@
+package Lesson7.clients;
+
+import Lesson7.apps.ClientApp;
+
+
+public class ClientOne {
+    public static void main(String[] args) {
+        new ClientApp();
+    }
+}
